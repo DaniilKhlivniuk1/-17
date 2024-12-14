a3cb7965c87c5caf4669ebded899e366c22be888
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2024-12-14 12:15:35 +0200

    hw

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//* 1
+{
+    const inputElement = document.querySelector(".input-1");
+    const buttonElement = document.querySelector(".btn-1");
+    inputElement.value = "BUTTON";
+    buttonElement.textContent = inputElement.value;
+  }
+  //* 2
+  {
+    const imgElement = document.querySelector(".img-2");
+    imgElement.src = "https://content2.rozetka.com.ua/banner_main/image/original/495819727.jpg";
+  }
+  //* 3
+  {
+    const linkElement = document.querySelector(".link-3");
+    const imgElement = document.querySelector(".img-3");
+    linkElement.src = "https://content.rozetka.com.ua/banner_main/image/original/494890259.jpg"
+  }
+  //* 4
+  {
+    const listElement = document.querySelector(".list-1");
+    listElement.firstElementChild.textContent = "New text";
+  }
+  
